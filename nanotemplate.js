2c7563675d14a7cb88af3e60542fff0e81f4eb86
Rui Lopes <ruidlopes@gmail.com>
2010-12-18 01:06:49 +0000

    minor tweak

--- nanotemplate.js
+++ nanotemplate.js
@@ -2,7 +2,7 @@ String.prototype._tregex = /(\$\w+)/g;
 
 String.prototype.template = String.prototype.t = function() {
 	if (arguments[0] instanceof Array && arguments[0][0])
-		return this.template(arguments[0][0]) + (arguments[0].length > 1 ? this.template(arguments[0].slice(1)) : "");
+		return this.t(arguments[0][0]) + (arguments[0].length > 1 ? this.t(arguments[0].slice(1)) : "");
 	else {
 		var args = typeof arguments[0] === "object" ? arguments[0] : arguments;
 		return this.replace(this._tregex, function(match) { return args[match.substr(1)]; });
